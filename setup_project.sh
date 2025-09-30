e4fc80c7702ea25725abc07685bd882576e24dd5
Xing Wang <xing@MacBookAir.attlocal.net>
2025-09-29 23:59:14 -0700

    inital commit of project files

--- setup_project.sh
+++ setup_project.sh
@@ -15,7 +15,7 @@ echo "Created files: .gitignore, requirements.txt"
 # Create files using "here-documents", EOF starts a here document
 
 cat > data/students.csv << 'EOF'
-name,age,grade, subject 
+name,age,grade,subject 
 1. Alice,20,5, science
 2. Bob,19,6, math 
 3. Charlie, 20, 4, history
